b5eb4bed2d642e05b46c9bab30f3e2d7eeb194af
Young Professor <youngprofessor991@gmail.com>
2023-11-19 17:38:46 +0300

    contraint to graduation added

--- graduation.js
+++ graduation.js
@@ -4,6 +4,36 @@ document.addEventListener('DOMContentLoaded', function () {
     var StudentId=''
     if (userData) {
         const user = JSON.parse(userData);
+        const CreditHoursDone=user[0].CreditsHoursDone
+        const TotalCreditHours=user[0].TotalCreditHours
+        const PhoneNumber=user[0].Phone
+        const remainingcredits= TotalCreditHours-CreditHoursDone
+        console.log("PhoneNumber= "+PhoneNumber);
+        console.log("PhoneNumber = " + PhoneNumber);
+        if (PhoneNumber === null) {
+            console.log("PhoneNumber is null");
+            document.getElementById('graduation-form').style.display = 'block';
+            document.getElementById('applicationDone').style.display = 'none';
+        } else {
+            document.getElementById('graduation-form').style.display = 'none';
+            document.getElementById('applicationDone').style.display = 'block';
+            console.log("PhoneNumber is not null");
+        }
+        
+
+        //if the user is not qualified
+        if (remainingcredits < 40) {
+            document.getElementById('graduation-form').style.display = 'block';
+            document.getElementById('not-qualified-message').style.display = 'none';
+           
+        } else {
+             const remainingHours=document.getElementById('chr')
+            remainingHours.textContent= `You have ${remainingcredits}  more credits hours to do. Happy Learning`
+            document.getElementById('graduation-form').style.display = 'none';
+            document.getElementById('not-qualified-message').style.display = 'block';
+        }
+        
+
         const welcomeMessage = document.getElementById("welcoming-guest");
         welcomeMessage.textContent = `Welcome ${user[0].FirstName}.`;
         const emailInput = document.getElementById('your-email');
@@ -40,9 +70,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 var data1 = [];
     
                 for (var i = 1; i < table1.rows.length; i++) {
-                    var courseCode = table1.rows[i].cells[0].querySelector("input").value;
-                    var courseTitle = table1.rows[i].cells[1].querySelector("input").value;
-                    var creditHours = table1.rows[i].cells[2].querySelector("input").value;
+                    var courseCode = table1.rows[i].cells[0].querySelector("select").value;
+                    var courseTitle = table1.rows[i].cells[1].querySelector("select").value;
+                    var creditHours = table1.rows[i].cells[2].querySelector("select").value;
                     if (courseCode.trim() !== "" || courseTitle.trim() !== "" || creditHours.trim() !== "") {
                         data1.push({
                             courseCode: courseCode,
@@ -52,15 +82,15 @@ document.addEventListener('DOMContentLoaded', function () {
                     };
                 }
                 // console.log("First Semester Courses");
-                // console.log(data1);
+                console.log(data1);
 
         var table2 = document.getElementById("second-semester");
                 var data2 = [];
     
                 for (var i = 1; i < table2.rows.length; i++) {
-                    var courseCode = table2.rows[i].cells[0].querySelector("input").value;
-                    var courseTitle = table2.rows[i].cells[1].querySelector("input").value;
-                    var creditHours = table2.rows[i].cells[2].querySelector("input").value;
+                    var courseCode = table2.rows[i].cells[0].querySelector("select").value;
+                    var courseTitle = table2.rows[i].cells[1].querySelector("select").value;
+                    var creditHours = table2.rows[i].cells[2].querySelector("select").value;
                     if (courseCode.trim() !== "" || courseTitle.trim() !== "" || creditHours.trim() !== "") {
                         data2.push({
                             courseCode: courseCode,
@@ -70,25 +100,43 @@ document.addEventListener('DOMContentLoaded', function () {
                     };
                 }
                 // console.log("Second Semester Courses");
-                // console.log(data2);
+                console.log(data2);
 
-        var table3 = document.getElementById("Inter-semester");
-                var data3 = [];
+        // var table3 = document.getElementById("Inter-semester");
+        //         var data3 = [];
     
-                for (var i = 1; i < table3.rows.length; i++) {
-                    var courseCode = table3.rows[i].cells[0].querySelector("input").value;
-                    var courseTitle = table3.rows[i].cells[1].querySelector("input").value;
-                    var creditHours = table3.rows[i].cells[2].querySelector("input").value;
-                    if (courseCode.trim() !== "" || courseTitle.trim() !== "" || creditHours.trim() !== "") {
-                        data3.push({
-                            courseCode: courseCode,
-                            courseTitle: courseTitle,
-                            creditHours: creditHours
-                        });
-                    };
-                }
+        //         for (var i = 1; i < table3.rows.length; i++) {
+        //             var courseCode = table3.rows[i].cells[0].querySelector("input").value;
+        //             var courseTitle = table3.rows[i].cells[1].querySelector("input").value;
+        //             var creditHours = table3.rows[i].cells[2].querySelector("input").value;
+        //             if (courseCode.trim() !== "" || courseTitle.trim() !== "" || creditHours.trim() !== "") {
+        //                 data3.push({
+        //                     courseCode: courseCode,
+        //                     courseTitle: courseTitle,
+        //                     creditHours: creditHours
+        //                 });
+        //             };
+        //         }
+
+                var table3 = document.getElementById("Inter-semester"); // Replace "yourTableId" with the actual ID of your table
+                    var data3 = [];
+
+                    for (var i = 1; i < table3.rows.length; i++) {
+                        var courseCode = table3.rows[i].cells[0].querySelector("select").value;
+                        var courseTitle = table3.rows[i].cells[1].querySelector("select").value;
+                        var creditHours = table3.rows[i].cells[2].querySelector("select").value;
+
+                        if (courseCode.trim() !== "" || courseTitle.trim() !== "" || creditHours.trim() !== "") {
+                            data3.push({
+                                courseCode: courseCode,
+                                courseTitle: courseTitle,
+                                creditHours: creditHours
+                            });
+                        }
+                    }
+
                 // console.log("Third Semester Courses");
-                // console.log(data3);
+                console.log(data3);
 
                 const HomeAddress= document.getElementById("Home-Address").value
                 const Phone= document.getElementById("phone").value
